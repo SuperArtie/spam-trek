4e5a282c091fb9cd917fc736b14f772c352925bc
Nathan Vasquez <nathan.superartie@gmail.com>
2014-10-20 15:33:19 -0500

    started falling blocks

--- www/js/main_controller.js
+++ www/js/main_controller.js
@@ -1,7 +1,37 @@
 (function(){
   'use strict';
   angular.module('spam-trek')
-  .controller('MainCtrl', ['$scope', function($scope){
+  .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
     $scope.title = 'spam-trek deluxe';
+    var bodyHeight    = window.innerHeight,
+        headerHeight  = document.getElementsByTagName('ion-header-bar')[0].clientHeight,
+        canvas        = document.getElementById('canvas'),
+        ctx           = canvas.getContext('2d'),
+        filters       = [];
+    canvas.height = bodyHeight - headerHeight;
+    canvas.width  = window.innerWidth;
+    $interval(filt, 1600);
+    draw();
+    function filt(){
+      for(var j = 0; j < 5; j++){
+        var randX = Math.floor((Math.random()*500)+100),
+            left   = randX,
+            top    = 0,
+            width  = 10,
+            length = 10;
+        filters.push({left:left, top:top, width:width, length:length});
+      }
+    }
+    function draw(){
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      for(var i = 0; i < filters.length; i++){
+        filters[i].top += 5;
+        ctx.rect(filters[i].left, filters[i].top, filters[i].width, filters[i].length);
+      }
+      ctx.closePath();
+      ctx.stroke();
+      window.requestAnimationFrame(draw);
+    }
   }]);
 })();
