15e73c565c9a14e5360cf1cd614077d9d9015c6e
Joy Pratt <joympratt@gmail.com>
2014-10-22 09:00:57 -0500

    catches spam

--- www/js/filter.js
+++ www/js/filter.js
@@ -8,8 +8,8 @@ var Filter = (function(){
     this.width  = 10;
     this.length = 10;
     this.speed  = Math.floor(Math.random()*4) + 1;
-    this.cX     = this.x + (this.width / 2);
-    this.cY     = this.y + (this.height / 2);
+    this.cX     = this.left + (this.width / 2);
+    this.cY;
   }
   Filter.prototype.draw = function(game){
     game.ctx.beginPath();
@@ -21,28 +21,18 @@ var Filter = (function(){
 
   Filter.prototype.catchSpam = function(spam){
     console.log('SPAM>>>>>>>', spam);
-    console.log('spam.cX in catchSpam', spam.cX);
-    console.log('spam.cY in catchSpam', spam.cY);
-    console.log('this.left in catchSpam', this.left);
-    console.log('this.top in catchSpam', this.top);
-    spam.r = spam.r * 1;
-    console.log = ('spam.r in catchSpam>>>>>', spam.r);
-    var topLeftSum = Math.pow((spam.cX - this.left), 2) + Math.pow((spam.cY - this.top), 2);
-    console.log = ('topLeftSum before comparison>>>>>', topLeftSum);
-    var topLeftDistance = Math.sqrt(topLeftSum);
-//        topLeftCatch    = topLeftDistance < (spam.r);
-//    console.log('topLeftDistance>>>>>>>>', topLeftDistance);
-//    console.log('topLeftCatch>>>>>>>>', topLeftCatch);
-//    var topRight = Math.sqrt(Math.pow(spam.cX - (this.x + 10), 2) + Math.pow(spam.cY - this.y, 2));
-//        topRight = topRight < (spam.r);
-//        console.log('topRight>>>>>>>', topRight);
-//    var bottomLeft = Math.sqrt(Math.pow(spam.cX - this.x, 2) + Math.pow(spam.cY - (this.y - 10), 2));
-//        bottomLeft = bottomLeft < (spam.r);
-//        console.log('bottomLeft>>>>>', bottomLeft);
-//    var bottomRight = Math.sqrt(Math.pow(spam.cX - (this.x + 10), 2) + Math.pow(spam.cY - (this.y - 10), 2));
-//        bottomRight = bottomRight < (spam.r);
-//        console.log('bottomRight>>>>', bottomRight);
-    return false;
+    console.log('this in catchSpam before cX, cY definition:', this);
+    console.log('spam.r', spam.r);
+    this.cX     = this.left + (this.width / 2);
+    this.cY     = this.top + (this.length / 2);
+    console.log('this in catchSpam after definition:', this);
+    var sumSquares = Math.pow((spam.cX - this.cX), 2) + Math.pow((spam.cY - this.cY), 2);
+    console.log('sumSquares in catchSpam>>>', sumSquares);
+    var distance = Math.sqrt(sumSquares);
+    console.log('distance in catchSpam>>>>>', distance);
+    var spamCaught = distance < spam.r;
+
+    return spamCaught;
   };
   return Filter;
 })();
